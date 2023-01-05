42e57b742a3b5de28503d977c744392e9a662d87
DanielYonzan <adtamang95@gmail.com>
2023-01-05 10:34:05 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-cg3y5n
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-cg3y5n)
---
+++ src/app/user/registration/registration.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css'],
+})
+export class RegistrationComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: [null, [Validators.required, Validators.maxLength(150)]],
+      password: [null, [Validators.required, Validators.maxLength(150)]],
+      username: [null, [Validators.required, Validators.maxLength(150)]],
+    });
+  }
+
+  create(event) {
+    console.log('form values' + JSON.stringify(this.registerForm.value));
+  }
+
+  ngOnInit() {}
+}
